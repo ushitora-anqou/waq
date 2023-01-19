f151dfebed401ad06ac702177964fbe8bcf206e8
Ushitora Anqou <ushitora@anqou.net>
2023-01-19 16:59:42 +0900

    Add db:reset subcommand

--- bin/main.ml
+++ bin/main.ml
@@ -1,39 +1,50 @@
 open Waq
-open Util
+open Util [@@warning "-33"]
 module C = Config
+
+let server () =
+  let host, port = (C.listen_host (), C.listen_port ()) in
+  Router.routes
+  |> Http.start_server ~host ~port @@ fun () ->
+     Log.info (fun m -> m "Listening on %s:%d" host port);
+     Lwt.return_unit
+
+let db_reset () =
+  let f =
+    (try%lwt Db.rollback () with _ -> Lwt.return_unit);%lwt
+    Db.migrate ();%lwt
+    let now = Ptime.now () in
+    let%lwt a =
+      let username = "foobar" in
+      let display_name = "Foobar's display name" in
+      let created_at, updated_at = (now, now) in
+      let private_key, public_key = Http.Signature.generate_keypair () in
+      let public_key = Http.Signature.encode_public_key public_key in
+      let private_key = Http.Signature.encode_private_key private_key in
+      let uri = Activity.url [ "users"; username ] in
+      let inbox_url = Activity.(uri ^/ "inbox") in
+      let followers_url = Activity.(uri ^/ "followers") in
+      Db.Account.make ~username ~public_key ~private_key ~display_name ~uri
+        ~inbox_url ~followers_url ~created_at ~updated_at ()
+      |> Db.Account.insert
+    in
+    assert (a.id = 1);
+    let%lwt _u =
+      let email = "foobar@example.com" in
+      let created_at, updated_at = (now, now) in
+      Db.User.make ~id:0 ~email ~created_at ~updated_at ~account_id:a.id
+      |> Db.User.insert
+    in
+    Lwt.return_unit
+  in
+  Lwt_main.run f
 
 let () =
   Log.(add_reporter (make_reporter ~l:Debug ()));
   C.load_file "config.yml";
   Http.Signature.initialize ();
   Db.initialize ();
-  let host, port = (C.listen_host (), C.listen_port ()) in
-  Router.routes
-  |> Http.start_server ~host ~port @@ fun () ->
-     Log.info (fun m -> m "Listening on %s:%d" host port);
-
-     (try%lwt Db.rollback () with _ -> Lwt.return_unit);%lwt
-     Db.migrate ();%lwt
-     let now = Ptime.now () in
-     let%lwt a =
-       let username = "foobar" in
-       let display_name = "Foobar's display name" in
-       let created_at, updated_at = (now, now) in
-       let private_key, public_key = Http.Signature.generate_keypair () in
-       let public_key = Http.Signature.encode_public_key public_key in
-       let private_key = Http.Signature.encode_private_key private_key in
-       let uri = Activity.url [ "users"; username ] in
-       let inbox_url = Activity.(uri ^/ "inbox") in
-       let followers_url = Activity.(uri ^/ "followers") in
-       Db.Account.make ~username ~public_key ~private_key ~display_name ~uri
-         ~inbox_url ~followers_url ~created_at ~updated_at ()
-       |> Db.Account.insert
-     in
-     assert (a.id = 1);
-     let%lwt _u =
-       let email = "foobar@example.com" in
-       let created_at, updated_at = (now, now) in
-       Db.User.make ~id:0 ~email ~created_at ~updated_at ~account_id:a.id
-       |> Db.User.insert
-     in
-     Lwt.return_unit
+  let subcommand =
+    if Array.length Sys.argv < 2 then "server" else Sys.argv.(1)
+  in
+  match subcommand with "db:reset" -> db_reset () | _ -> server ()
