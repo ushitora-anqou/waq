f7071973a1336a535b6d86860c61589969a290a7
Ushitora Anqou <ushitora@anqou.net>
2023-01-29 18:43:15 +0900

    Add test_util

---
+++ test/test_util.ml
@@ -0,0 +1,24 @@
+open Waq.Util
+
+let test_iota () = assert (iota 10 = [ 0; 1; 2; 3; 4; 5; 6; 7; 8; 9 ])
+
+let test_lwt_list_partition_map_p () =
+  let left, right =
+    Lwt_main.run
+      ([ 1; 2; 3; 4; 5 ]
+      |> Lwt_list.partition_map_p (fun x ->
+             if x mod 2 = 0 then Either.Left x |> Lwt.return
+             else Either.Right x |> Lwt.return))
+  in
+  assert (left = [ 2; 4 ]);
+  assert (right = [ 1; 3; 5 ]);
+  ()
+
+let () =
+  let open Alcotest in
+  run "util"
+    [
+      ("iota", [ test_case "case1" `Quick test_iota ]);
+      ( "lwt_list_partition_map_p",
+        [ test_case "case1" `Quick test_lwt_list_partition_map_p ] );
+    ]
