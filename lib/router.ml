b8c6968870d12ada5029df1a2b7bcdb738c6f0df
Ushitora Anqou <ushitora@anqou.net>
2023-02-03 13:00:12 +0900

    Simplify router

--- lib/router.ml
+++ lib/router.ml
@@ -13,32 +13,32 @@ let cors =
     ]
 
 let routes_from_servers =
-  [
-    get "/.well-known/host-meta" Controller.Well_known_host_meta.get;
-    get "/.well-known/webfinger" Controller.Well_known_webfinger.get;
-    get "/users/:name" Controller.Users.get;
-    post "/users/:name/inbox" Controller.Inbox.post;
-  ]
+  Controller.
+    [
+      get "/.well-known/host-meta" Well_known_host_meta.get;
+      get "/.well-known/webfinger" Well_known_webfinger.get;
+      get "/users/:name" Users.get;
+      post "/users/:name/inbox" Inbox.post;
+    ]
 
 let routes_from_clients =
-  [
-    post "/api/v1/accounts/:id/follow" Controller.Api_v1_accounts_follow.post;
-    post "/api/v1/accounts/:id/unfollow"
-      Controller.Api_v1_accounts_unfollow.post;
-    get "/api/v1/accounts/search" Controller.Api_v1_accounts_search.get;
-    post "/api/v1/statuses" Controller.Api_v1_statuses.post;
-    get "/api/v1/timelines/home" Controller.Api_v1_timelines_home.get;
-    post "/api/v1/apps" Controller.Api_v1_apps.post;
-    get "/oauth/authorize" Controller.Oauth_authorize.get;
-    post "/oauth/authorize" Controller.Oauth_authorize.post;
-    post "/oauth/token" Controller.Oauth_token.post;
-    get "/api/v1/apps/verify_credentials"
-      Controller.Api_v1_apps_verify_credentials.get;
-    get "/api/v1/streaming" Controller.Api_v1_streaming.get;
-    get "/api/v1/accounts/verify_credentials"
-      Controller.Api_v1_accounts_verify_credentials.get;
-    get "/api/v1/instance" Controller.Api_v1_instance.get;
-  ]
+  Controller.
+    [
+      post "/api/v1/accounts/:id/follow" Api_v1_accounts_follow.post;
+      post "/api/v1/accounts/:id/unfollow" Api_v1_accounts_unfollow.post;
+      get "/api/v1/accounts/search" Api_v1_accounts_search.get;
+      post "/api/v1/statuses" Api_v1_statuses.post;
+      get "/api/v1/timelines/home" Api_v1_timelines_home.get;
+      post "/api/v1/apps" Api_v1_apps.post;
+      get "/oauth/authorize" Oauth_authorize.get;
+      post "/oauth/authorize" Oauth_authorize.post;
+      post "/oauth/token" Oauth_token.post;
+      get "/api/v1/apps/verify_credentials" Api_v1_apps_verify_credentials.get;
+      get "/api/v1/streaming" Api_v1_streaming.get;
+      get "/api/v1/accounts/verify_credentials"
+        Api_v1_accounts_verify_credentials.get;
+      get "/api/v1/instance" Api_v1_instance.get;
+    ]
 
 let handler =
   let open Httpq.Server in
