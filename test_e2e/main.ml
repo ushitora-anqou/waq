e292e52cef659a08fea7fd5b61589b07a3c435f9
Ushitora Anqou <ushitora@anqou.net>
2023-02-05 00:35:32 +0900

    Add waq_mstdn_scenario_3 for reply test

--- test_e2e/main.ml
+++ test_e2e/main.ml
@@ -353,6 +353,71 @@ let waq_mstdn_scenario_2 waq_token mstdn_token =
 
   Lwt.return_unit
 
+let waq_mstdn_scenario_3 waq_token mstdn_token =
+  (* Lookup me from localhost:3000 *)
+  let%lwt aid, _, _ =
+    lookup `Mstdn ~token:mstdn_token ~username:"user1" ~domain:waq_server_domain
+      ()
+  in
+
+  (* Lookup @admin@localhost:3000 *)
+  let%lwt admin_id, _username, _acct =
+    lookup `Waq ~token:waq_token ~username:"admin" ~domain:"localhost:3000" ()
+  in
+
+  (* Follow @admin@localhost:3000 *)
+  follow `Waq ~token:waq_token admin_id;%lwt
+  Lwt_unix.sleep 1.0;%lwt
+
+  (* Follow me from @admin@localhost:3000 *)
+  follow `Mstdn ~token:mstdn_token aid;%lwt
+  Lwt_unix.sleep 1.0;%lwt
+
+  (* Post by me *)
+  let%lwt { uri; _ } = post `Waq ~token:waq_token () in
+  Lwt_unix.sleep 1.0;%lwt
+
+  (* Get home timeline of @admin@localhost:3000 and obtain the status's id *)
+  let%lwt id =
+    home_timeline `Mstdn ~token:mstdn_token >|= function
+    | [ `Assoc l ] ->
+        (* Check if the timeline is correct *)
+        assert (uri = (List.assoc "uri" l |> expect_string));
+        List.assoc "id" l |> expect_string
+    | _ -> assert false
+  in
+
+  (* Reply by @admin@localhost:3000 *)
+  let%lwt { uri; _ } = post `Mstdn ~token:mstdn_token ~in_reply_to_id:id () in
+  Lwt_unix.sleep 1.0;%lwt
+
+  (* Get home timeline of me and obtain the reply's id *)
+  let%lwt id =
+    home_timeline `Waq ~token:waq_token >|= function
+    | [ `Assoc l; `Assoc l2 ] ->
+        assert (uri = (List.assoc "uri" l |> expect_string));
+        assert (List.assoc "id" l2 = List.assoc "in_reply_to_id" l);
+        List.assoc "id" l |> expect_string
+    | _ -> assert false
+  in
+
+  (* Reply by me *)
+  let%lwt { uri; _ } = post `Waq ~token:waq_token ~in_reply_to_id:id () in
+  Lwt_unix.sleep 1.0;%lwt
+
+  (* Get home timeline of @admin@localhost:3000 and check *)
+  let%lwt _ =
+    home_timeline `Mstdn ~token:mstdn_token >|= function
+    | [ `Assoc l; `Assoc l2; `Assoc _ ] ->
+        (* Check if the timeline is correct *)
+        assert (uri = (List.assoc "uri" l |> expect_string));
+        assert (List.assoc "id" l2 = List.assoc "in_reply_to_id" l);
+        List.assoc "id" l |> expect_string
+    | _ -> assert false
+  in
+
+  Lwt.return_unit
+
 let waq_scenario_1 _waq_token =
   let%lwt access_token = fetch_access_token ~username:"user1" in
 
@@ -472,7 +537,11 @@ let waq_scenario_3 waq_token =
   Lwt.return_unit
 
 let scenarios_with_waq_and_mstdn () =
-  [ (1, waq_mstdn_scenario_1); (2, waq_mstdn_scenario_2) ]
+  [
+    (1, waq_mstdn_scenario_1);
+    (2, waq_mstdn_scenario_2);
+    (3, waq_mstdn_scenario_3);
+  ]
   |> List.iter @@ fun (i, scenario) ->
      Logq.debug (fun m -> m "===== Scenario waq-mstdn-%d =====" i);
      new_session @@ fun waq_token ->
