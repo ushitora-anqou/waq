85a3bae1c0c74866ff009b1ee4c86b6a4ae8746d
Ushitora Anqou <ushitora@anqou.net>
2023-02-09 20:45:43 +0900

    Simplify

--- lib/migration.ml
+++ lib/migration.ml
@@ -4,18 +4,17 @@ module type S = sig
 end
 
 let all : (int * (module S)) list =
-  [
-    (20221230220000, (module Migrate.M20221230_220000_create_accounts));
-    (20221230220001, (module Migrate.M20221230_220001_create_users));
-    (20221230220002, (module Migrate.M20221230_220002_create_statuses));
-    (20221230220003, (module Migrate.M20221230_220003_create_follows));
-    (20221230220004, (module Migrate.M20221230_220004_create_follow_requests));
-    (20230121195200, (module Migrate.M20230121_195200_create_oauth_applications));
-    ( 20230121195201,
-      (module Migrate.M20230121_195201_create_oauth_access_tokens) );
-    ( 20230122_183000,
-      (module Migrate.M20230122_183000_create_oauth_access_grants) );
-  ]
+  Migrate.
+    [
+      (20221230_220000, (module M20221230_220000_create_accounts));
+      (20221230_220001, (module M20221230_220001_create_users));
+      (20221230_220002, (module M20221230_220002_create_statuses));
+      (20221230_220003, (module M20221230_220003_create_follows));
+      (20221230_220004, (module M20221230_220004_create_follow_requests));
+      (20230121_195200, (module M20230121_195200_create_oauth_applications));
+      (20230121_195201, (module M20230121_195201_create_oauth_access_tokens));
+      (20230122_183000, (module M20230122_183000_create_oauth_access_grants));
+    ]
 
 let migrate () =
   Db.do_query @@ fun c ->
