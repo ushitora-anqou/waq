a59c36252cd7ebf18a92c3bfe396cbfc3677298a
Ushitora Anqou <ushitora@anqou.net>
2023-02-11 16:43:24 +0900

    Return real relationships from /api/v1/accounts/{follow,unfollow}

--- lib/controller/api_v1/accounts/unfollow.ml
+++ lib/controller/api_v1/accounts/unfollow.ml
@@ -1,29 +1,16 @@
-(* Recv POST /api/v1/accounts/:id/follow *)
-type post_api_v1_accounts_follow_res = {
-  id : string;
-  following : bool;
-  showing_reblogs : bool;
-  notifying : bool;
-  followed_by : bool;
-  blocking : bool;
-  blocked_by : bool;
-  muting : bool;
-  muting_notifications : bool;
-  requested : bool;
-  domain_blocking : bool;
-  endorsed : bool;
-}
-[@@deriving make, yojson { strict = false }]
+open Entity
+open Lwt.Infix
+open Helper
 
 (* Recv POST /api/v1/accounts/:id/unfollow *)
-(* Use post_api_v1_accounts_follow_res as a result *)
 let post req =
-  let%lwt self_id = Helper.authenticate_user req in
-  let id = req |> Httpq.Server.param ":id" |> Helper.int_of_string in
+  let%lwt self_id = authenticate_user req in
+  let id = req |> Httpq.Server.param ":id" |> int_of_string in
 
   (* Check if accounts are valid *)
   let%lwt self = Db.Account.get_one ~id:self_id () in
   let%lwt acc = Db.Account.get_one ~id () in
+
   (* Check if followed *)
   let%lwt f =
     Db.(
@@ -32,10 +19,7 @@ let post req =
   in
   (* If valid, send Undo of Follow to the server *)
   if f <> None then Service.Unfollow.kick self acc (Option.get f);
+
   (* Return the result to the client *)
-  make_post_api_v1_accounts_follow_res ~id:(string_of_int id) ~following:false
-    ~showing_reblogs:false ~notifying:false ~followed_by:false ~blocking:false
-    ~blocked_by:false ~muting:false ~muting_notifications:false ~requested:false
-    ~domain_blocking:false ~endorsed:false
-  |> post_api_v1_accounts_follow_res_to_yojson |> Yojson.Safe.to_string
-  |> Httpq.Server.respond ~headers:[ Helper.content_type_app_json ]
+  make_relationship_from_model self acc
+  >|= relationship_to_yojson >>= respond_yojson
