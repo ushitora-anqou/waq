d1432cc1377e2369f087ad3983cbaa19fcc46319
Ushitora Anqou <ushitora@anqou.net>
2023-02-26 12:24:38 +0900

    Use serialize_statuses to construct home timeline

--- lib/controller/api_v1/timelines/home.ml
+++ lib/controller/api_v1/timelines/home.ml
@@ -1,8 +1,8 @@
-open Entity
+open Helper
+open Lwt.Infix
 
 let parse_req req =
   let open Httpq.Server in
-  let open Helper in
   let%lwt self_id = Helper.authenticate_user req in
   let limit = req |> query ~default:"20" "limit" |> int_of_string in
   let limit = min limit 40 in
@@ -13,11 +13,9 @@ let parse_req req =
 let get req =
   let%lwt self_id, max_id, since_id, limit = parse_req req in
 
-  let conv (s : Db.Status.t) =
-    let open Lwt.Infix in
-    make_status_from_model ~self_id s >|= yojson_of_status
-  in
-  let%lwt statuses = Db.home_timeline ~id:self_id ~limit ~max_id ~since_id in
-  let%lwt statuses = Lwt_list.map_p conv statuses in
-  `List statuses |> Yojson.Safe.to_string
-  |> Httpq.Server.respond ~headers:[ Helper.content_type_app_json ]
+  Db.home_timeline ~id:self_id ~limit ~max_id ~since_id
+  >|= List.map (fun (s : Db.Status.t) -> s.id)
+  >>= Entity.serialize_statuses ~self_id
+  >|= List.map Entity.yojson_of_status
+  >|= (fun l -> `List l)
+  >>= respond_yojson
