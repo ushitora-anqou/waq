803be394ace5a6d0e3d0b2b5a0a4a80c7bea9ef7
Ushitora Anqou <ushitora@anqou.net>
2023-03-31 01:12:22 +0900

    Improve test

--- test_e2e/waq_mstdn_11_mention.ml
+++ test_e2e/waq_mstdn_11_mention.ml
@@ -1,76 +1,39 @@
 open Common2
 
-let f_ws (a0 : agent) (a1 : agent) =
-  let ws_notif = ref None in
-  let _, handler =
-    websocket_handler_state_machine ~init:`Recv
-      ~states:
-        [
-          ( `Recv,
-            fun l _ ->
-              assert (List.assoc "stream" l = `List [ `String "user" ]);
-              (match List.assoc "event" l |> expect_string with
-              | "notification" ->
-                  let payload =
-                    List.assoc "payload" l |> expect_string
-                    |> Yojson.Safe.from_string |> notification_of_yojson
-                  in
-                  ws_notif := Some payload
-              | _ -> ());
-              Lwt.return `Recv );
-        ]
-      ()
-  in
-
-  let rest_notif = ref None in
-  websocket a1 handler (fun pushf ->
-      (* a0: Post with mentions *)
-      let%lwt { uri; _ } =
-        post a0
-          ~content:(Printf.sprintf "@%s てすと" (acct_of_agent ~from:a0 a1))
-          ()
-      in
-      Lwt_unix.sleep 1.0;%lwt
-
-      (* a1: Check home timeline, which should be empty *)
-      let%lwt [] = home_timeline a1 in
-
-      (* a1: Check its notification *)
-      let%lwt [ n ] = get_notifications a1 in
-      assert ((Option.get n.status).uri = uri);
-      assert (n.account.acct = acct_of_agent ~from:a1 a0);
-
-      rest_notif := Some n;
-
-      pushf None);%lwt
-
-  assert (Option.is_some !ws_notif);
-  assert (Option.is_some !rest_notif);
-  assert (!ws_notif = !rest_notif);
-
-  Lwt.return_unit
-  [@@warning "-8"]
-
-let f (a0 : agent) (a1 : agent) =
+let f (a0 : agent) (a1 : agent) (a2 : agent) =
   (* a0: Post with mentions *)
   let%lwt { uri; _ } =
-    post a0 ~content:(Printf.sprintf "@%s てすと" (acct_of_agent ~from:a0 a1)) ()
+    post a0
+      ~content:
+        (Printf.sprintf "@%s @%s てすと"
+           (acct_of_agent ~from:a0 a1)
+           (acct_of_agent ~from:a0 a2))
+      ()
   in
   Lwt_unix.sleep 1.0;%lwt
 
   (* a1: Check home timeline, which should be empty *)
   let%lwt [] = home_timeline a1 in
 
+  (* a2: Check home timeline, which should be empty *)
+  let%lwt [] = home_timeline a2 in
+
   (* a1: Check its notification *)
   let%lwt [ n ] = get_notifications a1 in
   assert ((Option.get n.status).uri = uri);
   assert (n.account.acct = acct_of_agent ~from:a1 a0);
+
+  (* a2: Check its notification *)
+  let%lwt [ n ] = get_notifications a2 in
+  assert ((Option.get n.status).uri = uri);
+  assert (n.account.acct = acct_of_agent ~from:a2 a0);
 
   Lwt.return_unit
   [@@warning "-8"]
 
 let f_waq_mstdn =
   make_waq_and_mstdn_scenario @@ fun waq_token mstdn_token ->
+  let%lwt token2 = fetch_access_token ~username:"user2" in
   let a0 =
     make_agent ~kind:`Waq ~token:waq_token ~username:"user1"
       ~domain:waq_server_domain
@@ -79,11 +42,16 @@ let f_waq_mstdn =
     make_agent ~kind:`Mstdn ~token:mstdn_token ~username:"admin"
       ~domain:"localhost:3000"
   in
-  f a0 a1;%lwt
+  let a2 =
+    make_agent ~kind:`Waq ~token:token2 ~username:"user2"
+      ~domain:waq_server_domain
+  in
+  f a0 a1 a2;%lwt
   Lwt.return_unit
 
 let f_mstdn_waq =
   make_waq_and_mstdn_scenario @@ fun waq_token mstdn_token ->
+  let%lwt token2 = fetch_access_token ~username:"user2" in
   let a0 =
     make_agent ~kind:`Mstdn ~token:mstdn_token ~username:"admin"
       ~domain:"localhost:3000"
@@ -92,12 +60,17 @@ let f_mstdn_waq =
     make_agent ~kind:`Waq ~token:waq_token ~username:"user1"
       ~domain:waq_server_domain
   in
-  f_ws a0 a1;%lwt
+  let a2 =
+    make_agent ~kind:`Waq ~token:token2 ~username:"user2"
+      ~domain:waq_server_domain
+  in
+  f a0 a1 a2;%lwt
   Lwt.return_unit
 
 let f_waq_waq =
   make_waq_scenario @@ fun token ->
   let%lwt token2 = fetch_access_token ~username:"user2" in
+  let%lwt token3 = fetch_access_token ~username:"user3" in
   let a0 =
     make_agent ~kind:`Waq ~token ~username:"user1" ~domain:waq_server_domain
   in
@@ -105,4 +78,8 @@ let f_waq_waq =
     make_agent ~kind:`Waq ~token:token2 ~username:"user2"
       ~domain:waq_server_domain
   in
-  f_ws a0 a1
+  let a2 =
+    make_agent ~kind:`Waq ~token:token3 ~username:"user3"
+      ~domain:waq_server_domain
+  in
+  f a0 a1 a2
