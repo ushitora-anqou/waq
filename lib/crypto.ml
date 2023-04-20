51ac82400e80bae77e4127b7854111dbff448192
Ushitora Anqou <ushitora@anqou.net>
2023-04-20 22:33:26 +0900

    Use OAuth tokens without padding to avoid invalid WS stream

--- lib/crypto.ml
+++ lib/crypto.ml
@@ -6,5 +6,6 @@ module SecureRandom = struct
   let unique_token () =
     (* Thanks to: Doorkeeper::OAuth::Helpers::UniqueToken
        https://github.com/doorkeeper-gem/doorkeeper/blob/47037da4def738e4cfd930bd433f35629a5869f6/lib/doorkeeper/oauth/helpers/unique_token.rb *)
-    generate 32 |> Base64.encode_string ~alphabet:Base64.uri_safe_alphabet
+    generate 32
+    |> Base64.encode_string ~pad:false ~alphabet:Base64.uri_safe_alphabet
 end
