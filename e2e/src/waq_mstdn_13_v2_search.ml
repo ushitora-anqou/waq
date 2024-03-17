7c05e4af9d6c882f5bf35e58bb0f4bde5fb92f4f
Ushitora Anqou <ushitora@anqou.net>
2024-03-17 14:14:20 +0900

    e2e: enable waq-mstdn-13

--- e2e/src/waq_mstdn_13_v2_search.ml
+++ e2e/src/waq_mstdn_13_v2_search.ml
@@ -9,13 +9,15 @@ let f =
       Lwt.return_unit
   | _ -> assert false);%lwt
 
-  (* Lookup @admin@localhost:3000 without token, which should fail *)
-  (match%lwt search `Waq "@admin@localhost:3000" with
+  (* Lookup @mstdn1@mstdn_server_domain without token, which should fail *)
+  (match%lwt search `Waq ("@mstdn1@" ^ mstdn_server_domain) with
   | [], _, _ -> Lwt.return_unit
   | _ -> assert false);%lwt
 
   (* With token, it will succeed *)
-  (match%lwt search `Waq ~token:waq_token "@admin@localhost:3000" with
+  (match%lwt
+     search `Waq ~token:waq_token ("@mstdn1@" ^ mstdn_server_domain)
+   with
   | [ _ ], _, _ -> Lwt.return_unit
   | _ -> assert false);%lwt
 
