ebd747660adcca673f00c3e919ab8f81f0a77ede
Ushitora Anqou <ushitora@anqou.net>
2024-03-17 13:13:17 +0900

    e2e: enable waq-mstdn-6

--- e2e/src/waq_mstdn_6_fav.ml
+++ e2e/src/waq_mstdn_6_fav.ml
@@ -2,20 +2,21 @@ open Common
 
 let f =
   make_waq_and_mstdn_scenario @@ fun waq_token mstdn_token ->
-  (* Lookup @admin@localhost:3000 *)
-  let%lwt admin_id, _username, _acct =
-    lookup `Waq ~token:waq_token ~username:"admin" ~domain:"localhost:3000" ()
+  (* Lookup @mstdn1@mstdn_server_domain *)
+  let%lwt mstdn1_id, _username, _acct =
+    lookup `Waq ~token:waq_token ~username:"mstdn1" ~domain:mstdn_server_domain
+      ()
   in
-  (* Follow @admin@localhost:3000 *)
-  follow `Waq ~token:waq_token admin_id;%lwt
+  (* Follow @mstdn1@mstdn_server_domain *)
+  follow `Waq ~token:waq_token mstdn1_id;%lwt
   Lwt_unix.sleep 1.0;%lwt
 
-  (* Get user1's id on localhost:3000 *)
+  (* Get user1's id on mstdn_server_domain *)
   let%lwt mstdn_user1_id, _, _ =
     lookup `Mstdn ~token:mstdn_token ~username:"user1" ()
   in
 
-  (* Post by @admin@localhost:3000 *)
+  (* Post by @mstdn1@mstdn_server_domain *)
   let%lwt { id = mstdn_post_id; _ } = post `Mstdn ~token:mstdn_token () in
   Lwt_unix.sleep 1.0;%lwt
 
@@ -33,7 +34,7 @@ let f =
   let%lwt s = get_status `Waq ~token:waq_token id in
   assert s.favourited;
 
-  (* Check if the post is favourited in localhost:3000 *)
+  (* Check if the post is favourited in mstdn_server_domain *)
   (match%lwt get_favourited_by `Mstdn ~token:mstdn_token ~id:mstdn_post_id with
   | [ a ] ->
       assert (a.id = mstdn_user1_id);
