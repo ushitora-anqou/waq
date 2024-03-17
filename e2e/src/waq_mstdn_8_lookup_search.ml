2bd11245bb315a8896de16760d17c0a3f1ab6e0f
Ushitora Anqou <ushitora@anqou.net>
2024-03-17 13:22:33 +0900

    e2e: enable waq-mstdn-8

--- e2e/src/waq_mstdn_8_lookup_search.ml
+++ e2e/src/waq_mstdn_8_lookup_search.ml
@@ -2,21 +2,20 @@ open Common
 
 let f =
   make_waq_and_mstdn_scenario @@ fun waq_token mstdn_token ->
-  (* Lookup @admin@localhost:3000 *)
+  (* Lookup @mstdn1@mstdn_server_domain *)
   let%lwt res1 =
-    lookup `Waq ~token:waq_token ~username:"admin" ~domain:"localhost:3000" ()
+    lookup `Waq ~token:waq_token ~username:"mstdn1" ~domain:mstdn_server_domain
+      ()
   in
   let%lwt res2 =
-    lookup_via_v1_accounts_search `Waq ~token:waq_token ~username:"admin"
-      ~domain:"localhost:3000" ()
+    lookup_via_v1_accounts_search `Waq ~token:waq_token ~username:"mstdn1"
+      ~domain:mstdn_server_domain ()
   in
   let%lwt res3 =
-    lookup_via_v1_accounts_lookup `Waq ~token:waq_token ~username:"admin"
-      ~domain:"localhost:3000" ()
+    lookup_via_v1_accounts_lookup `Waq ~token:waq_token ~username:"mstdn1"
+      ~domain:mstdn_server_domain ()
   in
-  let%lwt res4 =
-    search `Waq ~token:waq_token "http://localhost:3000/users/admin"
-  in
+  let%lwt res4 = search `Waq ~token:waq_token (mstdn "/users/mstdn1") in
   assert (res1 = res2);
   assert (res1 = res3);
   assert (
@@ -26,8 +25,8 @@ let f =
 
   (* No token should cause an error *)
   expect_exc_lwt (fun () ->
-      lookup_via_v1_accounts_search `Waq ~username:"admin"
-        ~domain:"localhost:3000" ());%lwt
+      lookup_via_v1_accounts_search `Waq ~username:"mstdn1"
+        ~domain:mstdn_server_domain ());%lwt
 
   (* Lookup me *)
   let%lwt res1 = lookup `Waq ~token:waq_token ~username:"user1" () in
@@ -47,7 +46,7 @@ let f =
     | [ acct ], _, _ when res1 = (acct.id, acct.username, acct.acct) -> true
     | _ -> false);
 
-  (* Lookup post of @admin@localhost:3000 *)
+  (* Lookup post of @mstdn1@mstdn_server_domain *)
   let%lwt { uri; _ } = post `Mstdn ~token:mstdn_token () in
   let%lwt res = search `Waq ~token:waq_token uri in
   assert (
