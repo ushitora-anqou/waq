2d557e7b68944c905e647325aa86cfc699449299
Ushitora Anqou <ushitora@anqou.net>
2024-03-17 13:11:13 +0900

    e2e: enable waq-mstdn-5

--- e2e/src/main.ml
+++ e2e/src/main.ml
@@ -7,8 +7,8 @@ let all_tests =
     ("waq-mstdn-2", Waq_mstdn_2.f);
     ("waq-mstdn-3", Waq_mstdn_3_reply.f);
     ("waq-mstdn-4", Waq_mstdn_4_reblog.f);
-    (*
     ("waq-mstdn-5", Waq_mstdn_5_reblog.f);
+    (*
     ("waq-mstdn-6", Waq_mstdn_6_fav.f);
     ("waq-mstdn-7", Waq_mstdn_7_fav.f);
     ("waq-mstdn-8", Waq_mstdn_8_lookup_search.f);
