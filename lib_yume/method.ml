a398f7e2bdab723e8469c607518ad34575001414
Ushitora Anqou <ushitora@anqou.net>
2024-04-12 21:17:08 +0900

    use external yume

--- lib_yume/method.ml
+++
@@ -1,3 +0,0 @@
-type t = Cohttp.Code.meth
-
-let to_string = Cohttp.Code.string_of_method
--- lib_yume/status.ml
+++
@@ -1,5 +0,0 @@
-type t = Cohttp.Code.status_code
-
-let to_string = Cohttp.Code.string_of_status
-let is_error (s : t) = Cohttp.Code.(code_of_status s |> is_error)
-let is_success (s : t) = Cohttp.Code.(code_of_status s |> is_success)
