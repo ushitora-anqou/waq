c42a2da86b6109dfff93130af66480e4b2b3c10b
Ushitora Anqou <ushitora@anqou.net>
2026-09-01 10:11:29 +0000

    test(e2e): serve the OGP test page from the e2e cluster
    www.youtube.com responds to crawls from CI runner IPs with a redirect
    to www.google.com/sorry, which makes preview card generation fail
    sporadically and the 03-preview-card tests flaky. Serve a fixed page
    with Open Graph meta tags from an nginx pod inside the e2e cluster
    (ogp.waq-e2e.anqou.net) and point the tests at it instead.

--- e2e/src/waq_mstdn_14_preview_card.ml
+++ e2e/src/waq_mstdn_14_preview_card.ml
@@ -10,10 +10,9 @@ let get_preview_card_from_a1 ?(wait_for_card = false) env (a0 : agent) (a1 : age
 
   (* a1: Check the post. The post should have been fetched in advance because a1 follows a0.
 
-     Preview cards are generated asynchronously by crawling the linked page
-     (e.g. www.youtube.com), which can take more than the sleep above
-     depending on the external site's response time. When [wait_for_card] is
-     set, retry the search until the card shows up. *)
+     Preview cards are generated asynchronously by crawling the linked page,
+     which can take more than the sleep above. When [wait_for_card] is set,
+     retry the search until the card shows up. *)
   let rec loop i =
     let _, [ s ], _ = search env a1 uri in
     if (not wait_for_card) || Option.is_some s.card || i = 0 then s
@@ -25,7 +24,10 @@ let get_preview_card_from_a1 ?(wait_for_card = false) env (a0 : agent) (a1 : age
 [@@warning "-8"]
 
 let f_case1 env (a0 : agent) (a1 : agent) =
-  let url = "https://www.youtube.com/watch?v=OMv_EPMED8Y" in
+  (* The page is served by the ogp-server pod deployed in the e2e cluster:
+     external sites such as www.youtube.com block crawls from CI runner IPs
+     and make this test flaky. *)
+  let url = "http://ogp.waq-e2e.anqou.net/" in
   let s = get_preview_card_from_a1 ~wait_for_card:true env a0 a1 url in
   assert (Option.is_some s.card);
   let c = Option.get s.card in
